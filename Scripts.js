41e3f34e68114a04a722c6b924db5f31de013db4
EugenGlinsky <yauheni.hlinski@gmail.com>
2020-02-04 21:37:56 +0300

    Add autofill select

--- Scripts.js
+++ Scripts.js
@@ -1,21 +1,25 @@
-let Buidings;
-
-
-class Building {
-
-
-
+function initPage() {
+    $.getJSON("http://localhost:8080/Data.json", buildingFill);
+    $("#BuildingsSelect").on('change', function() {
+        profileFill(this.value);
+    });
 }
 
 
-function InitPage() {
-    BuildingFill();
+function buildingFill(data) {
+
+    window.data = data;
+    var selectList = $("#BuildingsSelect")
+    selectList.append($(new Option(" ", " ")))
+    data.Buildings.forEach(b => selectList.append($(new Option(b.name, b.id))));
+    selectList.val(0);
+
 }
 
-
-function BuildingFill() {
-    let buildingsSel = $("#BuildingsSelect");
-    buildingsSel.append($(new Option("Building1", "Building1")));
-    buildingsSel.append($(new Option("Building2", "Building2")));
-
+function profileFill(buildingId) {
+    var profiles = window.data.Buildings.find(e => e.id == buildingId).profiles;
+    var selectList = $("#profileSelect").find('option')
+        .remove()
+        .end();
+    profiles.forEach(p => selectList.append($(new Option(p.name, p.id))))
 }
