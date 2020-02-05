6f6908da1e0417972c822594dfc95e28871c55a0
EugenGlinsky <yauheni.hlinski@gmail.com>
2020-02-05 14:33:36 +0300

    all done with dictionary

--- Scripts.js
+++ Scripts.js
@@ -1,50 +1,80 @@
 function initPage() {
-    $.getJSON("http://localhost:8080/Data.json",
+    $.getJSON("http://localhost:8080/Buildings.json",
         function(value) {
-            buildingFill(value);
-            profileFill(undefined);
-            auditoriesFill(undefined);
+            buildingsRefresh(value);
+            profileRefresh();
+            auditoriesRefresh();
         });
-    $("#BuildingsSelect").on('change', function() {
-        profileFill(this.value);
 
+    $("#BuildingsSelect").on('change', function() {
+        profileRefresh(this.value);
+        auditoriesRefresh();
     });
+
     $("#ProfileSelect").on('change', function() {
-        auditoriesFill(this.value);
+        auditoriesRefresh(this.value);
     });
 }
 
-
-function buildingFill(data) {
-    window.data = data;
+function buildingsRefresh(data) {
     var selectList = $("#BuildingsSelect")
     selectList.append($(new Option(" ", "")))
-    data.Buildings.forEach(b => selectList.append($(new Option(b.name, b.id))));
+    $.each(data, function(key, value) {
+        selectList.append($(new Option(value.Name, value.Id)));
+    })
     selectList.val(0);
-
 }
 
-function profileFill(buildingId) {
+function profileRefresh(selectedBuiding) {
+
+    if (!window.Profiles) {
+        $.getJSON("http://localhost:8080/Profiles.json",
+            function(value) {
+                window.Profiles = value;
+                fillProfiles(selectedBuiding);
+            });
+    } else {
+        fillProfiles(selectedBuiding);
+    }
+}
+
+function fillProfiles(selectedBuiding) {
     var selectList = $("#ProfileSelect").find('option')
         .remove()
         .end();
-    selectList.append($(new Option("Все", "0")))
-    if (buildingId) {
-        var profiles = window.data.Buildings.find(e => e.id == buildingId).profiles;
-        profiles.forEach(p => selectList.append($(new Option(p.name, p.id))));
+    selectList.append($(new Option("Все", "")))
+    if (selectedBuiding) {
+        $.each(window.Profiles[selectedBuiding], function(key, value) {
+            selectList.append($(new Option(value.Name, value.Id)));
+        })
     }
     selectList.val();
 }
 
-function auditoriesFill(profileId) {
+
+function auditoriesRefresh(selProfileId) {
+
+    if (!window.Auditories) {
+        $.getJSON("http://localhost:8080/Rooms.json",
+            function(value) {
+                window.Auditories = value;
+                fillAuditories(selProfileId);
+            });
+    } else {
+        fillAuditories(selProfileId);
+    }
+}
+
+function fillAuditories(selProfileId) {
     var selectList = $("#RoomSelect").find('option')
         .remove()
         .end();
     selectList.append($(new Option("Все", "")))
-    if (profileId) {
-        var currentBuildingId = $("#BuildingsSelect").val();
-        var auditories = window.data.Buildings.find(e => e.id == currentBuildingId).profiles.find(p => p.id == profileId).rooms;
-        auditories.forEach(a => selectList.append($(new Option(a.number, a.id))));
+
+    if (selProfileId) {
+        $.each(window.Auditories[selProfileId], function(key, value) {
+            selectList.append($(new Option(value.Number, value.Id)));
+        })
     }
     selectList.val();
 }
