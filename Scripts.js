89c926fb238a0570b818fd48583949c647de56bc
EugenGlinsky <yauheni.hlinski@gmail.com>
2020-02-10 18:38:39 +0300

    transfer to api

--- Scripts.js
+++ Scripts.js
@@ -1,56 +1,98 @@
 $(document).ready(initPage);
 
+window.sortParam = {};
+window.sortDictionary = {
+    1: "name",
+    2: "audience",
+    3: "profile",
+    4: "needBel"
+}
+
 
 function initPage() {
-    $.getJSON("http://localhost:8080/Buildings.json",
+    $.getJSON("https://lyceumexams.herokuapp.com/api/dictionary",
+        function(value) {
+            window.dictionary = value;
+        });
+
+
+
+    $.getJSON("https://lyceumexams.herokuapp.com/api/corpses",
         function(value) {
             window.buildings = value;
             buildingsRefresh(value);
             profilesRefresh();
             auditoriesRefresh();
         });
-    $.getJSON("http://localhost:8080/Students.json",
-        function(value) {
-            window.students = value;
-            window.sortParam = {};
-            fillStudentsTable();
-        });
 
-    $("#BuildingsSelect").on('change', function() {
+    $("#BuildingsSelect").on('change', function(event) {
+        var index = event.target.value;
+
+        window.currentBuildingIndex = index;
+        setStudents()
         profilesRefresh(this.value);
         auditoriesRefresh();
     });
 
     $("#ProfileSelect").on('change', function() {
+        window.currentProfileIndex = this.value;
         auditoriesRefresh(this.value);
+        setStudents()
+    });
+    $("#RoomSelect").on('change', function() {
+        window.currentRoomIndex = this.value;
     });
 
     $("#StudentsTable thead tr").on('click', function(event) {
         sortTable(event)
     });
+}
 
+function setStudents() {
+    if (currentBuildingIndex) {
+        var currentBuildg = buildings[currentBuildingIndex];
+        var alias = currentBuildg.alias;
+        var place = currentProfileIndex ? currentBuildg.places[currentProfileIndex]._id : "";
+        $.getJSON(`https://lyceumexams.herokuapp.com/api/pupils?corps=${alias}&place=${place} `,
+            function(value) {
+                window.students = value;
+                fillStudentsTable();
+            });
+    } else {
+        students = null;
+        fillStudentsTable();
+    }
 }
 
 function buildingsRefresh(buildings) {
     var buildingSelect = $("#BuildingsSelect");
-    buildingSelect.append($(new Option(" ", "")))
-    $.each(buildings, function(key) {
-        buildingSelect.append($(new Option(key, key)));
+    buildingSelect.append($(new Option(" ", "")));
+
+    $.each(buildings, function(index, element) {
+        buildingSelect.append($(new Option(element.name, index)));
     });
-    buildingSelect.val(0);
+    buildingSelect.val();
 }
 
 function profilesRefresh(selectedBuiding) {
+
     var profileSelect = $("#ProfileSelect");
     profileSelect.find('option')
         .remove()
         .end();
     profileSelect.append($(new Option("Все", "")))
+
     if (selectedBuiding) {
-        $.each(window.buildings[selectedBuiding], function(key) {
-            profileSelect.append($(new Option(key, key)));
+        var places = buildings[selectedBuiding].places;
+
+        $.each(places, function(key, value) {
+            profileSelect.append($(new Option(value.code, key)));
         })
+    } else {
+        currentProfileIndex = "";
     }
+
+
     profileSelect.val();
 }
 
@@ -62,11 +104,12 @@ function auditoriesRefresh(selProfileId) {
         .remove()
         .end();
     roomSelect.append($(new Option("Все", "")))
-
     if (selProfileId) {
-        $.each(window.buildings[buildingSelect.val()][selProfileId], function(key, value) {
-            roomSelect.append($(new Option(value, value)));
+        $.each(buildings[currentBuildingIndex].places[currentProfileIndex].audience, function(key, value) {
+            roomSelect.append($(new Option(value.name, value._id)));
         })
+    } else {
+        currentRoomIndex = "";
     }
     roomSelect.val();
 }
@@ -76,16 +119,30 @@ function auditoriesRefresh(selProfileId) {
 function fillStudentsTable() {
 
     let tableBody = $("#StudentsTable").find("tbody").empty();
+    if (students != null) {
+        var rows = "";
+        $.each(students, function(key, value) {
+            var auditoryId = value.audience;
+            var profileId = value.profile;
 
-    $.each(students, function(key, value) {
-        tableBody.append(`<tr>+
+            rows += `<tr draggable="true" >+
             <td>${++key}</td>
-            <td>${value.name}</td>
-            <td>${value.room}</td>
-            <td>${value.profile}</td>
-            <td ${value.isBel ? "class=\"bel\"":"" }></td>
-        </tr>`)
-    });
+            <td>${value.firstName} ${value.lastName} ${value.parentName}</td>
+            <td>${dictionary.audiences[auditoryId]}</td>
+            <td>${dictionary.profiles[profileId]}</td>
+            <td ${value.needBel ? "class=\"bel\"":"" }></td>
+        </tr>`
+        });
+        tableBody.append(rows);
+    }
+}
+
+function fillAuditoriesTable() {
+
+    let tableBody = $("#AuditoriesTable").find("tbody").empty();
+
+
+
 }
 
 function sortTable(event) {
@@ -103,20 +160,19 @@ function sortTable(event) {
         prevDirectionElement.classList.add("hidden");
     }
 
-
-
-
     if (currentIndex == prevIndex) {
 
         sortParam.descDirection = !sortParam.descDirection;
-        sortStudentsByColumn(currentIndex, sortParam.descDirection)
+
 
     } else {
         sortParam.descDirection = false;
-        sortStudentsByColumn(currentIndex, sortParam.descDirection);
 
     }
+
+    sortStudentsByColumn(currentIndex, sortParam.descDirection);
     fillStudentsTable();
+
     currentDirectionElement = currentElement.getElementsByTagName("span")[0];
     currentDirectionElement.dataset.active = 1;
     currentDirectionElement.classList.remove("hidden");
@@ -125,11 +181,23 @@ function sortTable(event) {
 
 function sortStudentsByColumn(sortColumn, desc = false) {
 
-    var propertyName = Object.keys(students[0])[sortColumn - 1];
+    var propertyName = sortDictionary[sortColumn];
 
     students.sort(function(a, b) {
-        let tmpValA = a[propertyName];
-        let tmpValB = b[propertyName];
+        let tmpValA;
+        let tmpValB;
+        if (propertyName == "name") {
+            tmpValA = a["firstName"] + a["lastName"] + a["parentName"];
+            tmpValB = b["firstName"] + b["lastName"] + b["parentName"];
+
+        } else if (propertyName == "needBel") {
+            tmpValA = a[propertyName];
+            tmpValB = b[propertyName];
+        } else {
+            tmpValA = dictionary[propertyName + "s"][a[propertyName]];
+            tmpValB = dictionary[propertyName + "s"][b[propertyName]];
+        }
+
 
         if (tmpValA > tmpValB) return desc ? -1 : 1;
         if (tmpValA == tmpValB) return 0;
