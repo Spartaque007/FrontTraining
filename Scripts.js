fb76ae153d3f88765f30b2a72008b57ef317753f
EugenGlinsky <yauheni.hlinski@gmail.com>
2020-02-11 17:09:09 +0300

    refactoring Complete

--- Scripts.js
+++ Scripts.js
@@ -1,13 +1,24 @@
 $(document).ready(initPage);
 
-window.sortParam = {};
-window.sortDictionary = {
+var state = {
+    currentBuilding: "",
+    currentProfile: "",
+    currentAuditoryId: "",
+};
+var students = null;
+
+var sortParam = {
+    descDirection: true,
+    sortIndex: "",
+    activeDirectionElement: null,
+    filterText: ""
+};
+var sortDictionary = {
     1: "name",
     2: "audience",
     3: "profile",
     4: "needBel"
 }
-
 
 function initPage() {
     $.getJSON("https://lyceumexams.herokuapp.com/api/dictionary",
@@ -15,113 +26,137 @@ function initPage() {
             window.dictionary = value;
         });
 
-
-
     $.getJSON("https://lyceumexams.herokuapp.com/api/corpses",
         function(value) {
             window.buildings = value;
-            buildingsRefresh(value);
+            buildingsRefresh();
             profilesRefresh();
-            auditoriesRefresh();
+            auditoriesRefresh()
         });
-
     $("#BuildingsSelect").on('change', function(event) {
-        var index = event.target.value;
+        state.currentBuilding = event.target.value;
+        state.currentProfile = "";
+        state.currentAuditoryId = "";
 
-        window.currentBuildingIndex = index;
-        setStudents()
-        profilesRefresh(this.value);
+        profilesRefresh();
+        auditoriesRefresh()
+        setStudents();
+        hidePrevDirectionElement();
+    });
+    $("#ProfileSelect").on('change', function(event) {
+        state.currentProfile = event.target.value;
         auditoriesRefresh();
+        setStudents();
+        hidePrevDirectionElement();
     });
-
-    $("#ProfileSelect").on('change', function() {
-        window.currentProfileIndex = this.value;
-        auditoriesRefresh(this.value);
-        setStudents()
+    $("#RoomSelect").on('change', function(event) {
+        state.currentAuditoryId = event.target.value;
+        fillStudentsTable(students);
     });
-    $("#RoomSelect").on('change', function() {
-        window.currentRoomIndex = this.value;
-    });
-
     $("#StudentsTable thead tr").on('click', function(event) {
         sortTable(event)
     });
+
+    $("#SearchInput").on('input', function(event) {
+        sortParam.filterText = event.target.value;
+        fillStudentsTable(students);
+    });
+
 }
 
-function setStudents() {
-    if (currentBuildingIndex) {
-        var currentBuildg = buildings[currentBuildingIndex];
-        var alias = currentBuildg.alias;
-        var place = currentProfileIndex ? currentBuildg.places[currentProfileIndex]._id : "";
-        $.getJSON(`https://lyceumexams.herokuapp.com/api/pupils?corps=${alias}&place=${place} `,
-            function(value) {
-                window.students = value;
-                fillStudentsTable();
-            });
-    } else {
-        students = null;
-        fillStudentsTable();
-    }
-}
-
-function buildingsRefresh(buildings) {
+function buildingsRefresh() {
     var buildingSelect = $("#BuildingsSelect");
     buildingSelect.append($(new Option(" ", "")));
-
     $.each(buildings, function(index, element) {
         buildingSelect.append($(new Option(element.name, index)));
     });
     buildingSelect.val();
 }
 
-function profilesRefresh(selectedBuiding) {
-
+function profilesRefresh() {
     var profileSelect = $("#ProfileSelect");
     profileSelect.find('option')
         .remove()
         .end();
     profileSelect.append($(new Option("Все", "")))
 
-    if (selectedBuiding) {
-        var places = buildings[selectedBuiding].places;
-
+    if (state.currentBuilding) {
+        var places = buildings[state.currentBuilding].places;
         $.each(places, function(key, value) {
             profileSelect.append($(new Option(value.code, key)));
-        })
-    } else {
-        currentProfileIndex = "";
+        });
     }
-
-
     profileSelect.val();
 }
 
+function auditoriesRefresh() {
 
-function auditoriesRefresh(selProfileId) {
+    var auditories = null;
+    if (state.currentBuilding && state.currentProfile) {
+        auditories = buildings[state.currentBuilding].places[state.currentProfile].audience;
+    } else if (state.currentBuilding) {
+        auditories = [];
+        buildings[state.currentBuilding].places.forEach(function(item, i, arr) {
+            auditories = auditories.concat(item.audience);
+        });
+    }
+    fillAuditoriesSelect(auditories);
+    fillAuditoriesTable(auditories);
+}
+
+function fillAuditoriesSelect(auditories) {
     var roomSelect = $("#RoomSelect");
-    var buildingSelect = $("#BuildingsSelect");
     roomSelect.find('option')
         .remove()
         .end();
-    roomSelect.append($(new Option("Все", "")))
-    if (selProfileId) {
-        $.each(buildings[currentBuildingIndex].places[currentProfileIndex].audience, function(key, value) {
+    roomSelect.append($(new Option("Все", "")));
+    if (auditories != null) {
+        $.each(auditories, function(key, value) {
             roomSelect.append($(new Option(value.name, value._id)));
         })
-    } else {
-        currentRoomIndex = "";
     }
     roomSelect.val();
 }
 
+function fillAuditoriesTable(auditories) {
+    let tableBody = $("#AuditoriesTable").find("tbody").empty();
+    var rows = "";
 
+    $.each(auditories, function(key, value) {
+        rows += `<tr>+
+        <td>${value.name}</td>
+        <td>${value.count}</td>
+        <td>${value.max}</td>
+        <td ${value.bel ? "class=\"bel\"":"" }></td>
+    </tr>`
+    });
+    tableBody.append(rows);
+}
 
-function fillStudentsTable() {
+function setStudents() {
+    if (state.currentBuilding) {
+        building = buildings[state.currentBuilding];
+        var alias = building.alias;
+        var place = state.currentProfile ? building.places[state.currentProfile]._id : "";
+        $.getJSON(`https://lyceumexams.herokuapp.com/api/pupils?corps=${alias}&place=${place} `,
+            function(value) {
+                students = value;
+                fillStudentsTable(students);
+            });
+    } else {
+        students = null;
+        fillStudentsTable(students);
+    }
+}
 
+function fillStudentsTable(students) {
     let tableBody = $("#StudentsTable").find("tbody").empty();
-    if (students != null) {
+
+    var filteredStudents = applyFilters();
+
+    if (filteredStudents != null) {
         var rows = "";
-        $.each(students, function(key, value) {
+        $.each(filteredStudents, function(key, value) {
             var auditoryId = value.audience;
             var profileId = value.profile;
 
@@ -137,50 +172,32 @@ function fillStudentsTable() {
     }
 }
 
-function fillAuditoriesTable() {
-
-    let tableBody = $("#AuditoriesTable").find("tbody").empty();
-
-
-
-}
-
 function sortTable(event) {
-
+    if (students == null) return;
     var currentElement = event.target.cellIndex == undefined ? event.target.parentElement : event.target;
     var currentIndex = currentElement.cellIndex;
     if (currentIndex == 0) return;
 
-    var prevIndex = sortParam.currentCell;
-
-    var prevDirectionElement = document.querySelector("span[data-active='1']");
-    if (prevDirectionElement != null) {
-        prevDirectionElement.dataset.active = 0;
-        prevDirectionElement.classList.remove(sortParam.descDirection ? "directn-desc" : "directn-asc");
-        prevDirectionElement.classList.add("hidden");
-    }
-
-    if (currentIndex == prevIndex) {
-
-        sortParam.descDirection = !sortParam.descDirection;
-
-
-    } else {
-        sortParam.descDirection = false;
-
-    }
-
+    hidePrevDirectionElement()
+    var isColumnNotChanged = (sortParam.sortIndex == currentIndex);
+    sortParam.descDirection = isColumnNotChanged ? !sortParam.descDirection : isColumnNotChanged;
     sortStudentsByColumn(currentIndex, sortParam.descDirection);
-    fillStudentsTable();
+    fillStudentsTable(students);
 
     currentDirectionElement = currentElement.getElementsByTagName("span")[0];
-    currentDirectionElement.dataset.active = 1;
     currentDirectionElement.classList.remove("hidden");
     currentDirectionElement.classList.add(sortParam.descDirection ? "directn-desc" : "directn-asc");
+    sortParam.activeDirectionElement = currentDirectionElement;
+}
+
+function hidePrevDirectionElement() {
+    if (sortParam.activeDirectionElement != null) {
+        sortParam.activeDirectionElement.classList.remove(sortParam.descDirection ? "directn-desc" : "directn-asc");
+        sortParam.activeDirectionElement.classList.add("hidden");
+    }
 }
 
 function sortStudentsByColumn(sortColumn, desc = false) {
-
     var propertyName = sortDictionary[sortColumn];
 
     students.sort(function(a, b) {
@@ -204,6 +221,47 @@ function sortStudentsByColumn(sortColumn, desc = false) {
         if (tmpValA < tmpValB) return desc ? 1 : -1;
     });
 
-    window.sortParam.currentCell = sortColumn;
-    window.sortParam.descDirection = desc;
+    sortParam.sortIndex = sortColumn;
+    sortParam.descDirection = desc;
+}
+
+function applyFilters() {
+
+    var filteredStudents = filterStudentsTableByRoom(state.currentAuditoryId);
+
+    var filteredStudents = filterTableByName(sortParam.filterText, filteredStudents);
+    return filteredStudents;
+}
+
+function filterTableByName(inputText, students) {
+    if (students == null) return null;
+
+    if (!inputText) {
+        return students;
+    }
+
+    let tmpStudentsArray = [];
+    inputText = inputText.toLowerCase();
+
+    students.forEach(function(item) {
+        if (item.firstName.toLowerCase().includes(inputText)) {
+            tmpStudentsArray.push(item);
+        }
+    });
+    return tmpStudentsArray;
+}
+
+function filterStudentsTableByRoom(roomId) {
+    if (roomId) {
+        let tmpStudents = [];
+        students.forEach(function(item, index) {
+            if (item.audience == roomId) {
+                tmpStudents.push(item);
+            }
+        });
+        return tmpStudents;
+
+    } else {
+        return students;
+    }
 }
